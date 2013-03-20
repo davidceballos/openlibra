d6a3a7960687fd41c081cf4feddead0dacc434fa
davidceballos <forimontellano@gmail.com>
2013-03-20 09:24:16 +0100

    Update id.py

--- id.py
+++ id.py
@@ -1,5 +1,7 @@
 import requests
+import json
+
 resid = requests.get("http://openlibra.com/api/v1/get/?", params={"id":"589"})
-print resid.text
-
-
+cadena = resid.text[2:-3]
+autor = json.loads(cadena)["author"]
+print autor
