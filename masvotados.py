d89eced5dc1beed8ae9b1a13974037f3157b7e33
davidceballos <forimontellano@gmail.com>
2013-04-11 13:21:58 +0300

    Update masvotados.py

--- masvotados.py
+++ masvotados.py
@@ -1,4 +1,16 @@
 import requests
-masvotado = requests.get("http://openlibra.com/api/v1/get/?", params={"criteria":"most_voted","num_items":"10"})
-print masvotado.text
+import json
 
+masvotado = requests.get("http://openlibra.com/api/v1/get/?", params={"criteria":"most_voted","num_items":"10"})
+sincorchetes = masvotado.text[2:-5]
+sincomas = sincorchetes.split(']},')
+
+final = []
+
+for i in sincomas:
+    final.append(i+"]}")
+
+    
+for i in xrange(0,9):
+    print json.loads(final[i])["author"]
+    print json.loads(final[i])["ID"]
