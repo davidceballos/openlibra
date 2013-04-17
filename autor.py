0492509670d521363a88b9c295e4c828a989659c
davidceballos <forimontellano@gmail.com>
2013-04-17 09:55:14 +0300

    Update autor.py

--- autor.py
+++ autor.py
@@ -1,4 +1,16 @@
 import requests
-autor = requests.get("http://openlibra.com/api/v1/get/?", params={"book_autor":"adrian_paenza"})
-print autor.text
+import json
 
+import requests
+autor = requests.get("http://openlibra.com/api/v1/get/?", params={"book_autor":"adrian_paenza"})
+sincorchetes = autor.text[2:-5]
+sincomas = sincorchetes.split(']},')
+
+final = []
+
+for i in sincomas:
+    final.append(i+"]}")
+    
+for i in final:
+    print json.loads(i)["title"]
+    print json.loads(i)["ID"]
