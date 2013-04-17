53a4f487b5568693fa4ccc86cd4634f3916ba186
davidceballos <forimontellano@gmail.com>
2013-04-17 10:03:23 +0300

    Update masvistoporcate.py

--- masvistoporcate.py
+++ masvistoporcate.py
@@ -1,4 +1,17 @@
 import requests
-categoria = requests.get("http://openlibra.com/api/v1/get/?", params={"category":"libros_programacion","criteria":"most_viewed"})
-print categoria.text
+import json
 
+categoria = requests.get("http://openlibra.com/api/v1/get/?", params={"category":"libros_aspecotos_legales","criteria":"most_viewed"})
+
+sincorchetes = categoria.text[2:-5]
+sincomas = sincorchetes.split(']},')
+
+final = []
+
+for i in sincomas:
+    final.append(i+"]}")
+    
+for i in final:
+    print json.loads(i)["author"]
+    print json.loads(i)["title"]
+    print json.loads(i)["ID"]
